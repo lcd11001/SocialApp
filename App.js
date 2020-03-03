b074aa87a0c1493268309c76578cf05e0fc0d21e
Luong Cong Dan <lcd11001@gmail.com>
2020-03-03 10:05:24 +0700

    + App tabs icon

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+import React from 'react'
+
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import LoadingScreen from './screens/LoadingScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+
 import HomeScreen from './screens/HomeScreen'
+import MessageScreen from './screens/MessageScreen'
+import NotificationScreen from './screens/NotificationScreen'
+import PostScreen from './screens/PostScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 import * as firebase from 'firebase'
 
@@ -21,20 +30,90 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-const AppStack = createStackNavigator({
-    Home: HomeScreen
-})
+// const AppStack = createStackNavigator({
+//     Home: HomeScreen
+// })
+
+const AppTabs = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons name="ios-home" size={24} color={tintColor} />
+                )
+            }
+        },
+        Message: {
+            screen: MessageScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-chatboxes"
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Post: {
+            screen: PostScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-add-circle"
+                        size={48}
+                        color="#E9446A"
+                        style={{
+                            shadowColor: '#E9446A',
+                            shadowOffset: { width: 0, height: 0 },
+                            shadowRadius: 10,
+                            shadowOpacity: 0.3
+                        }}
+                    />
+                )
+            }
+        },
+        Notification: {
+            screen: NotificationScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-notifications"
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons name="ios-person" size={24} color={tintColor} />
+                )
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: '#161F3D',
+            inactiveTintColor: '#B8BBC4',
+            showLabel: false
+        }
+    }
+)
 
 const AuthStack = createStackNavigator({
     Login: LoginScreen,
-    Register: RegisterScreen,
+    Register: RegisterScreen
 })
 
 export default createAppContainer(
     createSwitchNavigator(
         {
             Loading: LoadingScreen,
-            App: AppStack,
+            App: AppTabs,
             Auth: AuthStack
         },
         {
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    LayoutAnimation
+} from 'react-native'
 import * as firebase from 'firebase'
 
 export default class HomeScreen extends React.Component {
@@ -21,6 +27,8 @@ export default class HomeScreen extends React.Component {
     }
 
     render() {
+        LayoutAnimation.easeInEaseOut()
+
         return (
             <View style={styles.container}>
                 <Text>
