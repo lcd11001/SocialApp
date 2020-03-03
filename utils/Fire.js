2d3e418eb4284c0c50e75d9f3f3f523e4c01ced0
Luong Cong Dan <lcd11001@gmail.com>
2020-03-03 11:44:20 +0700

    + firebase util

---
+++ utils/Fire.js
@@ -0,0 +1,70 @@
+import { firebaseConfig } from '../config'
+import firebase from 'firebase'
+
+class Fire {
+    constructor() {
+        firebase.initializeApp(firebaseConfig)
+    }
+
+    addPost = async ({ text, localUri }) => {
+        const remoteUri = await this.uploadPhotoAsync(localUri)
+
+        return new Promise((res, rej) => {
+            this.firestore
+                .collection('post')
+                .add({
+                    text: text,
+                    uid: this.uid,
+                    timestamp: this.timestamp,
+                    image: remoteUri
+                })
+                .then(ref => {
+                    res(ref)
+                })
+                .catch(err => {
+                    rej(err)
+                })
+        })
+    }
+
+    uploadPhotoAsync = async uri => {
+        const path = `photo/${this.uid}/${this.timestamp}.jpg`
+
+        return new Promise(async (res, rej) => {
+            const response = await fetch(uri)
+            const file = await response.blob()
+
+            let upload = firebase
+                .storage()
+                .ref(path)
+                .put(file)
+
+            upload.on(
+                'state_changed',
+                snapshot => {},
+                error => {
+                    rej(error)
+                },
+                async () => {
+                    const url = await upload.snapshot.ref.getDownloadURL()
+                    res(url)
+                }
+            )
+        })
+    }
+
+    get firestore() {
+        return firebase.firestore()
+    }
+
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid
+    }
+
+    get timestamp() {
+        return Date.now()
+    }
+}
+
+Fire.shared = new Fire()
+export default Fire
